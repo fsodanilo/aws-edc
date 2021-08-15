9fb8fef7ed4c55c0a2d5a3c278ca01b9ba5fdbb7
Danilotve <fsodanilo@gmail.com>
2021-08-15 18:04:11 -0300

    first Delta

---
+++ infrastructure/iam.tf
@@ -0,0 +1,77 @@
+resource "aws_iam_role" "lambda" {
+  name = "BRONXLambdaRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": "AssumeRole"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    IES   = "BRONXHOME",
+    CURSO = "EDC"
+  }
+
+}
+
+
+
+resource "aws_iam_policy" "lambda" {
+  name        = "BRONXAWSLambdaBasicExecutionRolePolicy"
+  path        = "/"
+  description = "Provides write permissions to CloudWatch Logs, S3 buckets and EMR Steps"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticmapreduce:*"
+            ],
+            "Resource": "*"
+        },
+        {
+          "Action": "iam:PassRole",
+          "Resource": ["arn:aws:iam::127012818163:role/EMR_DefaultRole",
+                       "arn:aws:iam::127012818163:role/EMR_EC2_DefaultRole"],
+          "Effect": "Allow"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.lambda.arn
+}
+
