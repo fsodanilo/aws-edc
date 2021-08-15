3f5af6c1a2d8833b6b17f48b9a74068b4c4c6c0b
Danilotve <fsodanilo@gmail.com>
2021-08-15 18:24:49 -0300

    add user permissão add role e policy

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -60,8 +60,8 @@ resource "aws_iam_policy" "lambda" {
         },
         {
           "Action": "iam:PassRole",
-          "Resource": ["arn:aws:iam::127012818163:role/EMR_DefaultRole",
-                       "arn:aws:iam::127012818163:role/EMR_EC2_DefaultRole"],
+          "Resource": ["arn:aws:iam::284165992806:role/EMR_DefaultRole",
+                       "arn:aws:iam::284165992806:role/EMR_EC2_DefaultRole"],
           "Effect": "Allow"
         }
     ]
