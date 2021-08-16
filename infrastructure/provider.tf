9e2aecc36132ab379d00d40eb23f403083120d1d
Danilotve <fsodanilo@gmail.com>
2021-08-16 15:25:51 -0300

    add recurso kinesis

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,9 +5,9 @@ provider "aws" {
 # centralizar o controle de estado do terraform
 
 terraform {
-    backend "s3" {
-        bucket = "terraform-state-edc"
-        key    = "state/edc/terraform.tfstate"
-        region = "us-east-2"
-    }
+  backend "s3" {
+    bucket = "terraform-state-edc"
+    key    = "state/edc/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
