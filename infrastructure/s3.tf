9446acbc287e1cb6fc13e5c143d65c14fc2c6022
Danilotve <fsodanilo@gmail.com>
2021-08-16 15:10:06 -0300

    Add bucket para o kinesis

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -15,3 +15,21 @@ resource "aws_s3_bucket" "dl" {
     }
   }
 }
+
+resource "aws_s3_bucket" "dl" {
+  bucket = "datalake-brx-streaming-bucket"
+  acl    = "private"
+
+  tags = {
+    IES   = "BRONXHOME",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
